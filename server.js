6814e55ce5369f43edd7b39e84ceaa7aa6f054ac
AntCamper <152764140+AntCamper@users.noreply.github.com>
2024-04-28 13:15:05 -0700

    used port for the local host instead setup the routes for html index and notes

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const html_routes = require('Develop\public\routes\html_routes.js');
-const api_routes = require('Develop\public\routes\api_routes.js') ;
+const html_routes = require('./Develop/public/routes/html_routes.js');
+const api_routes = require('./Develop/public/routes/api_routes.js');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
-app.use(express,json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // Corrected typo here
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(html_routes);
+app.use(api_routes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:3000`)
-})
+    console.log(`Server running on http://localhost:${PORT}`); 
+});
